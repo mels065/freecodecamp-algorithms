a764a797db1680cf9000025d3bf9cc9e55116851
Brandon Mellus <mellusbrandon@gmail.com>
2017-12-15 18:28:34 -0500

    Add 'Smallest Common Multiple' challenge

---
+++ intermediate/ch15/smallest-common-multiple.js
@@ -0,0 +1,21 @@
+function smallestCommons(arr) {
+  const min = Math.min(...arr), max = Math.max(...arr);
+  return Array.apply(null, {length: (max - min) + 1}).map((_, i) => min + i)
+    // https://en.wikipedia.org/wiki/Least_common_multiple#Reduction_by_the_greatest_common_divisor
+    .reduce((current, next) => Math.abs(current * next) / euclidean(current, next));
+
+
+  // https://en.wikipedia.org/wiki/Euclidean_algorithm
+  function euclidean(a, b) {
+    if (a < b) {
+      const temp = a;
+      a = b;
+      b = temp;
+    }
+    const r = a % b;
+    return r ? euclidean(b, r) : b;
+  }
+}
+
+
+smallestCommons([0]);
